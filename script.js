d038ea7be524e782c90780a4165946ce703197cf
ABDOU <abdoukichenama@gmail.com>
2022-03-24 15:21:43 +0100

    modification (fonctionnel)

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-let users = ['Marks', 'James', 'Peter', 'Jane', 'Mary'];
-function addUsers(){
-    let template = users.map(user => `<li>${user}</li>`).join('\n');
-    document.querySelector('ul').innerHTML = template;
-}
-addUsers();
-let btnAdd = document.querySelector('button');
-let input = document.querySelector('input');
+// let users = ['Marks', 'James', 'Peter', 'Jane', 'Mary'];
+// function addUsers(){
+//     let template = users.map(user => `<li>${user}</li>`).join('\n');
+//     document.querySelector('ul').innerHTML = template;
+// }
+// addUsers();
+
+let ul = document.getElementById('liste');
+let btnAdd = document.getElementById('toto');
+let input = document.getElementById('ecrire');
 btnAdd.addEventListener('click', () =>{
-    users.push(input.value);
+    let li = document.createElement('li');
+    li.innerText = input.value;
+    ul.appendChild(li);
     input.value = '';
-    addUsers();        
+    console.log();       
 });
